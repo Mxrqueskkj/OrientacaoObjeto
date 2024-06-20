9c7c7184c6f0f94940476125493d397a719b2978
Micael Marques <micaelf.yo18@gmail.com>
2024-06-20 12:55:41 -0300

    commit atualização do app de mensagens

---
+++ basico/src/MSNMensseger/ComputadorDoPedrinho.java
@@ -0,0 +1,10 @@
+package MSNMensseger;
+
+public class ComputadorDoPedrinho {
+    public static void main(String[] args) {
+        MSNMensseger msn = new MSNMensseger();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
---
+++ basico/src/MSNMensseger/MSNMensseger.java
@@ -0,0 +1,19 @@
+package MSNMensseger;
+
+public class MSNMensseger {
+    public void enviarMensagem(){
+        validarConectadoInternet();
+        System.out.println("Enviando mensagem");
+        salvarHistoricoMensagem();
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem");
+        salvarHistoricoMensagem();
+    }
+    private void validarConectadoInternet(){
+        System.out.println("Validando se está conectado a internet");
+    }
+    private void salvarHistoricoMensagem(){
+        System.out.println("Salvando o historico da mensagem");
+    }
+}
